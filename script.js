f025e91bb7788ba84cd072eb5f7d8de1496d7b61
Nicolas Bourg <nicolas.bourg.dev@gmail.com>
2022-01-30 16:52:01 +0100

    html et css done

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const score = document.getElementById('score');
+const days = document.getElementById('days');
+const endScreen = document.getElementById('endScreen');
+
+function virusPop() {
+    let virus = new Image();
+
+    virus.src = "./assets/img/virus.png"
+}
